fb550598a460b4a9d5242b5ee923159922e757f6
Thiago Alexandre dos Santos <thiagoas@ciandt.com>
2022-12-07 17:27:00 -0300

    setup: Added PrimeVUE

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/arya-blue/theme.css';
+import 'primeicons/primeicons.css';
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 import router from './router'
 
 import './assets/main.css'
@@ -9,6 +14,7 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(PrimeVue);
+app.use(router);
 
 app.mount('#app')
